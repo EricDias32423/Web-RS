a66155a568ada28122e534d1a1ff00a8d1e6e5e2
Terceiro <ericdias.senaidstatui@gmail.com>
2026-02-10 15:35:06 -0300

    Criação das view pagina inicial e cadastro_ong, e criação do layout para nav e aba de pesquisa, unico arquivo com html é o cadastro, com ons inputs e botão

---
+++ resources/views/cadastro_ong.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
+    <title>Cadastro de Ong</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Cadastre sua Ong Aqui!</h1>
+        <div class="row mt-4">
+            <div class="col-lg-7 col-md-7 col-sm-12">
+                <label for="">Nome da Ong</label>
+                <input type="text" id="nome">
+            </div>
+            <div class="col-lg-7 col-md-7 col-sm-12">
+                <label for="">Email</label>
+                <input type="text" id="email">
+            </div>
+            <div class="col-lg-7 col-md-7 col-sm-12">
+                <label for="">Descrição</label>
+                <input type="text" id="descrição">
+            </div>
+
+            <div>
+                <button type="submit">Cadastrar</button>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
