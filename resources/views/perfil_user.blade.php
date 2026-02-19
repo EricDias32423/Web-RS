92ab9f043fe10af696308605a33c6bab8f22414e
Terceiro <ericdias.senaidstatui@gmail.com>
2026-02-19 16:00:03 -0300

    criar as principais paginas, alterar, deletar. visualizar, e configurei o css

---
+++ resources/views/perfil_user.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    @vite(['resources/css/nav.css'])
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Perfil de Usuário</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <nav id="nav-bar">
+                <p>eae</p>
+            </nav>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::view('/Cadastrar', 'cadastrar_ong');
+Route::view('/perfil', 'perfil_user');
